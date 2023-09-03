69d003df9d77be87444372b71d6157868504cd73
Farhan Ainurrahman <farhanainurr@student.ub.ac.id>
2023-09-03 21:55:05 +0700

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,54 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {navLinks} from "../data/index";
+import { navLinks } from "../data/index";
 import { NavLink } from "react-router-dom";
-const navbar = () => {
+import { useState, useEffect } from "react";
+
+const NavbarComponent = () => {
+  const [changeColor, setChangeColor] = useState(false);
+
+  const backgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+  useEffect(() => {
+    backgroundColor();
+
+    window.addEventListener("scroll", backgroundColor);
+  });
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
       <Container>
-        <Navbar.Brand href="#home">F-Course</Navbar.Brand>
+        <Navbar.Brand href="#home" className="fs-3 fw-bold">
+          F-Course
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            {/* digunakan untuk menambahkan link pada navba sesuai yang ada di data */}
             {navLinks.map((link) => {
               return (
                 <div className="nav-link" key={link.id}>
-                  <NavLink to={link.path} >{link.text}</NavLink>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive, isPending }) =>
+                      isPending ? "pending" : isActive ? "active" : ""
+                    }
+                    end
+                  >
+                    {link.text}
+                  </NavLink>
                 </div>
               );
             })}
           </Nav>
           <div className="masuk">
-            <button>
+            <button className="btn btn-outline-danger rounded-1 ps-4 pe-4">
               Masuk
             </button>
           </div>
@@ -30,4 +58,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default NavbarComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <div className="homePage">
       <header className="w-100 min-vh-100"></header>
+      <div  className="w-100 min-vh-100"></div>
     </div>
   )
 }
